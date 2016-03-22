f379623bb66687d44016617026be5d449e809155
Whoin Lee <whoinlee@gmail.com>
2016-03-22 17:30:24 -0400

    Added files via upload

---
+++ twitteroauth/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'ubr6TmhS6aWQtQvbMeCEchLiO');
+define('CONSUMER_SECRET', 'fGww6mxhF307GkC7FDNvQY96l9pzQ9KNoASyXqM669Clmuitui');
+define('OAUTH_CALLBACK', 'http://www.whoin.net/tweetTweetCalls/twitteroauth/callback.php');
